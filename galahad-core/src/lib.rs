7a4960fe36d8a484dad7ff0fc96d1e874eb972f6
nathan2slime <nathan3boss@gmail.com>
2026-08-28 19:53:19 +0000

    feat: add core password service contract

--- galahad-core/src/lib.rs
+++ galahad-core/src/lib.rs
@@ -3,6 +3,7 @@
 mod credential;
 mod error;
 mod repository;
+mod service;
 mod session;
 mod user;
 
@@ -11,5 +12,6 @@ pub use error::AuthError;
 pub use repository::{
     BoxRepositoryFuture, CredentialRepository, RepositoryResult, SessionRepository, UserRepository,
 };
+pub use service::{BoxServiceFuture, PasswordService, ServiceResult};
 pub use session::{Session, SessionId};
 pub use user::{User, UserId};
