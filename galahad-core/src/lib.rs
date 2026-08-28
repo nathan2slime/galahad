d15ae030dc42c91593d2d5b286dfbdc8486060cb
Jhonathan <nathan3boss@gmail.com>
2026-08-28 16:49:57 -0300

    feat: add core repository traits (#3)

--- galahad-core/src/lib.rs
+++ galahad-core/src/lib.rs
@@ -2,10 +2,14 @@
 
 mod credential;
 mod error;
+mod repository;
 mod session;
 mod user;
 
 pub use credential::PasswordCredential;
 pub use error::AuthError;
+pub use repository::{
+    BoxRepositoryFuture, CredentialRepository, RepositoryResult, SessionRepository, UserRepository,
+};
 pub use session::{Session, SessionId};
 pub use user::{User, UserId};
